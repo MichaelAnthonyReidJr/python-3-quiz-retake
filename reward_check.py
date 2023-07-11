96d7ae1a677c3432ef687acada1015b8eaa5bcd4
MichaelAnthonyReidJr <EfficiencyMarriesEfficacy@gmail.com>
2023-07-11 19:07:33 -0400

    Add files

--- reward_check.py
+++ reward_check.py
@@ -1,10 +1,31 @@
 
 def find_best_user(file_path):
+    name = " "
+    max = 0
+    
+    with open (file_path, 'r') as file:
+        for eachline in file:
+            lines = file.readline()
+            words = lines.split(",")
+            print(words)
+            
+            for i in range(len(words)):
+                print(words)
+                
+                if words[5] > max:
+                    max = int(words[5])
+                    #print(words[5])
+                    name = words[0]
+                return name
+             
+    
+    
+    
     pass #TODO
 
 
 def test():
     result = find_best_user("users.txt")
-    print(result)
+    print(f"Congraatulations {result}! You have a lot of reward points, would you like to spend them?")
 
 test()
